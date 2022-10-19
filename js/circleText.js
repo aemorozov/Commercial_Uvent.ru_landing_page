007cb015eb44aa1fa83a1b088b82eadd666d0162
aemorozov <mclenina19@gmail.com>
2022-10-19 19:48:11 +0400

    add block -Kak mu rabotaem- and fix bugs

--- js/circleText.js
+++ js/circleText.js
@@ -1,8 +1,8 @@
-document.querySelector('.block4-2-circle1-text').innerHTML = "СВАДЬБЫ   СВАДЬБЫ   СВАДЬБЫ   СВАДЬБЫ   СВАДЬБЫ".split('')
-.map((e,i) => `<span style="--rot:${i*7.2}deg">${e}</span>`).join('');
+document.querySelector('.block4-2-circle1-text').innerHTML = "С В А Д Ь Б Ы    С В А Д Ь Б Ы    С В А Д Ь Б Ы    С В А Д Ь Б Ы    С В А Д Ь Б Ы".split('')
+.map((e,i) => `<span class="circle-text" style="--rot:${i*4.2}deg">${e}</span>`).join('');
 
-document.querySelector('.block4-2-circle2-text').innerHTML = "ВЫПУСКНЫЕ  ВЫПУСКНЫЕ  ВЫПУСКНЫЕ  ВЫПУСКНЫЕ".split('')
-.map((e,i) => `<span style="--rot:${i*8.2}deg">${e}</span>`).join('');
+document.querySelector('.block4-2-circle2-text').innerHTML = "В Ы П У С К Н Ы Е   В Ы П У С К Н Ы Е   В Ы П У С К Н Ы Е   В Ы П У С К Н Ы Е".split('')
+.map((e,i) => `<span class="circle-text" style="--rot:${i*4.5}deg">${e}</span>`).join('');
 
 document.querySelector('.block4-2-circle3-text').innerHTML = "ДНИ  РОЖДЕНИЯ  ДНИ  РОЖДЕНИЯ  ДНИ  РОЖДЕНИЯ".split('')
-.map((e,i) => `<span style="--rot:${i*8}deg">${e}</span>`).join('');
+.map((e,i) => `<span class="circle-text" style="--rot:${i*8}deg">${e}</span>`).join('');
