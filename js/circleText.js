50befe2ac1a8105bfd302dc8477fe48d2f67692c
aemorozov <mclenina19@gmail.com>
2022-10-27 13:50:10 +0400

    chore: optimized imgs and rewrite circleText.js

--- js/circleText.js
+++ js/circleText.js
@@ -1,26 +1,55 @@
-document.querySelector(".block4-2-circle1-text").innerHTML =
-  "С В А Д Ь Б Ы    С В А Д Ь Б Ы    С В А Д Ь Б Ы    С В А Д Ь Б Ы    С В А Д Ь Б Ы"
-    .split("")
-    .map(
-      (e, i) =>
-        `<span class="circle-text" style="--rot:${i * 4.2}deg">${e}</span>`
-    )
-    .join("");
+const makeCircleText = (blockClass, text) => {
+  const rotation = 360 / (text.length + 2);
 
-document.querySelector(".block4-2-circle2-text").innerHTML =
-  "В Ы П У С К Н Ы Е   В Ы П У С К Н Ы Е   В Ы П У С К Н Ы Е   В Ы П У С К Н Ы Е"
+  document.querySelector(blockClass).innerHTML = text
     .split("")
     .map(
-      (e, i) =>
-        `<span class="circle-text" style="--rot:${i * 4.5}deg">${e}</span>`
+      (symbol, index) =>
+        `<span class="circle-text" style="--rot:${
+          index * rotation
+        }deg">${symbol}</span>`
     )
     .join("");
+};
 
-document.querySelector(".block4-2-circle3-text").innerHTML =
-  "ДНИ  РОЖДЕНИЯ  ДНИ  РОЖДЕНИЯ  ДНИ  РОЖДЕНИЯ"
-    .split("")
-    .map(
-      (e, i) =>
-        `<span class="circle-text" style="--rot:${i * 8}deg">${e}</span>`
-    )
-    .join("");
+makeCircleText(
+  ".block4-2-circle1-text",
+  "С В А Д Ь Б Ы    С В А Д Ь Б Ы    С В А Д Ь Б Ы    С В А Д Ь Б Ы    С В А Д Ь Б Ы "
+);
+
+makeCircleText(
+  ".block4-2-circle2-text",
+  "В Ы П У С К Н Ы Е   В Ы П У С К Н Ы Е   В Ы П У С К Н Ы Е   В Ы П У С К Н Ы Е "
+);
+
+makeCircleText(
+  ".block4-2-circle3-text",
+  "Д Н И  Р О Ж Д Е Н И Я  Д Н И  Р О Ж Д Е Н И Я  Д Н И  Р О Ж Д Е Н И Я "
+);
+
+// document.querySelector(".block4-2-circle1-text").innerHTML =
+//   "С В А Д Ь Б Ы    С В А Д Ь Б Ы    С В А Д Ь Б Ы    С В А Д Ь Б Ы    С В А Д Ь Б Ы"
+//     .split("")
+//     .map(
+//       (e, i) =>
+//         `<span class="circle-text" style="--rot:${i * 4.2}deg">${e}</span>`
+//     )
+//     .join("");
+
+// document.querySelector(".block4-2-circle2-text").innerHTML =
+//   "В Ы П У С К Н Ы Е   В Ы П У С К Н Ы Е   В Ы П У С К Н Ы Е   В Ы П У С К Н Ы Е"
+//     .split("")
+//     .map(
+//       (e, i) =>
+//         `<span class="circle-text" style="--rot:${i * 4.5}deg">${e}</span>`
+//     )
+//     .join("");
+
+// document.querySelector(".block4-2-circle3-text").innerHTML =
+//   "ДНИ  РОЖДЕНИЯ  ДНИ  РОЖДЕНИЯ  ДНИ  РОЖДЕНИЯ"
+//     .split("")
+//     .map(
+//       (e, i) =>
+//         `<span class="circle-text" style="--rot:${i * 8}deg">${e}</span>`
+//     )
+//     .join("");
