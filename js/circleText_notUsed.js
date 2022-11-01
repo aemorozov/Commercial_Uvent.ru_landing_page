71f26617d80a4e2b3be30c750f5c08fd64343d73
aemorozov <mclenina19@gmail.com>
2022-11-01 14:49:33 +0400

    chore: rewrite circleText script for more DRY

--- js/circleText_notUsed.js
+++ js/circleText_notUsed.js
@@ -1,31 +1,58 @@
-const makeCircleText = (blockClass, text) => {
-  const rotation = 360 / text.length;
+const text = document.getElementsByClassName("text");
 
-  document.querySelector(blockClass).innerHTML = text
-    .split("")
-    .map(
-      (symbol, index) =>
-        `<span class="circle-text" style="--rot:${
-          index * rotation
-        }deg">${symbol}</span>`
-    )
-    .join("");
-};
+function makeCircleText(text) {
+  let rotation = 0;
 
-makeCircleText(
-  ".block4-2-circle1-text",
-  "С В А Д Ь Б Ы    С В А Д Ь Б Ы    С В А Д Ь Б Ы    С В А Д Ь Б Ы    С В А Д Ь Б Ы   "
-);
+  let textForTransform = "";
 
-makeCircleText(
-  ".block4-2-circle2-text",
-  "В Ы П У С К Н Ы Е   В Ы П У С К Н Ы Е   В Ы П У С К Н Ы Е   В Ы П У С К Н Ы Е   "
-);
+  for (i = 0; i < text.length; i++) {
+    textForTransform = text[i].outerText;
 
-makeCircleText(
-  ".block4-2-circle3-text",
-  "Д Н И  Р О Ж Д Е Н И Я  Д Н И  Р О Ж Д Е Н И Я  Д Н И  Р О Ж Д Е Н И Я   "
-);
+    console.log(text[i].outerText.length);
+
+    rotation = 360 / (text[i].outerText.length + 1);
+
+    text[i].innerHTML = text[i].innerHTML
+      .split("")
+      .map(
+        (symbol, index) =>
+          `<span class="circle-text" style="--rot:${
+            index * rotation
+          }deg">${symbol}</span>`
+      )
+      .join("");
+  }
+}
+
+makeCircleText(text);
+
+// const makeCircleText = (blockClass, text) => {
+//   const rotation = 360 / text.length;
+
+//   document.querySelector(blockClass).innerHTML = text
+//     .split("")
+//     .map(
+//       (symbol, index) =>
+//         `<span class="circle-text" style="--rot:${index * rotation
+//         }deg">${symbol}</span>`
+//     )
+//     .join("");
+// };
+
+// makeCircleText(
+//   ".block4-2-circle1-text",
+//   "С В А Д Ь Б Ы    С В А Д Ь Б Ы    С В А Д Ь Б Ы    С В А Д Ь Б Ы    С В А Д Ь Б Ы   "
+// );
+
+// makeCircleText(
+//   ".block4-2-circle2-text",
+//   "В Ы П У С К Н Ы Е   В Ы П У С К Н Ы Е   В Ы П У С К Н Ы Е   В Ы П У С К Н Ы Е   "
+// );
+
+// makeCircleText(
+//   ".block4-2-circle3-text",
+//   "Д Н И  Р О Ж Д Е Н И Я  Д Н И  Р О Ж Д Е Н И Я  Д Н И  Р О Ж Д Е Н И Я   "
+// );
 
 // document.querySelector(".block4-2-circle1-text").innerHTML =
 //   "С В А Д Ь Б Ы    С В А Д Ь Б Ы    С В А Д Ь Б Ы    С В А Д Ь Б Ы    С В А Д Ь Б Ы"
