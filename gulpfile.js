fd192fb014752c9e587c17b53b1b456eb14d1940
Andrey Edounov <opty@opty.ru>
2022-11-06 17:09:28 +0300

    chore: improved gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,7 @@
+const autoprefixer = require("gulp-autoprefixer");
 const browserSync = require("browser-sync");
 const { sync } = require("del");
 const gulp = require("gulp");
-const autoprefixer = require("gulp-autoprefixer");
 const cssMinify = require("gulp-clean-css");
 const gcssmq = require("gulp-group-css-media-queries");
 const gulpSass = require("gulp-sass");
@@ -10,62 +10,66 @@ const sass = require("sass");
 const htmlMinify = require("gulp-htmlmin");
 const imagemin = require("gulp-imagemin");
 
-const gulpSassWorker = gulpSass(sass);
+const distDir = "./dist/";
+const srcDir = "./src/";
 
-const dist = "./dist/";
-const src = "./src/";
+const htmlDir = "html/";
+const htmlFiles = `${srcDir}${htmlDir}*.html`;
 
-const images = "img/";
-const html = "html/";
-const styles = "styles/";
-const js = "js/";
+const jsDir = "js/";
+const jsFiles = `${srcDir}${jsDir}*.js`;
 
-const htmlAll = `${src}${html}*.html`;
-const jsAll = `${src}${js}*.js`;
-const stylesAll = `${src}${styles}*.scss`;
+const stylesDir = "styles/";
+const styleFiles = `${srcDir}${stylesDir}*.scss`;
+
+const imagesDir = "img/";
+const imageFiles = `${srcDir}${imagesDir}**/*`;
 
 const processHTML = () => {
   return gulp
-    .src(htmlAll)
+    .src(htmlFiles)
     .pipe(htmlMinify({ collapseWhitespace: true, removeComments: true }))
-    .pipe(gulp.dest(dist))
+    .pipe(gulp.dest(distDir))
     .pipe(browserSync.reload({ stream: true }));
 };
 
+const gulpSassWorker = gulpSass(sass);
+
 const processStyles = () => {
   return gulp
-    .src(stylesAll)
+    .src(styleFiles)
     .pipe(gulpSassWorker().on("error", gulpSassWorker.logError))
     .pipe(autoprefixer({ grid: true }))
     .pipe(gcssmq())
     .pipe(cssMinify())
-    .pipe(gulp.dest(dist))
+    .pipe(gulp.dest(distDir))
     .pipe(browserSync.stream());
 };
 
 const processJS = () => {
   return gulp
-    .src(jsAll)
+    .src(jsFiles)
     .pipe(jsMinify())
-    .pipe(gulp.dest(`${dist}${js}`))
+    .pipe(gulp.dest(`${distDir}${jsDir}`))
     .pipe(browserSync.stream());
 };
 
 const processImages = () => {
   return gulp
-    .src(`${src}${images}**/*`)
+    .src(imageFiles)
     .pipe(imagemin())
-    .pipe(gulp.dest(`${dist}${images}`));
+    .pipe(gulp.dest(`${distDir}${imagesDir}`));
 };
 
 const clean = async () => {
-  return sync(dist, { force: true });
+  return sync(distDir, { force: true });
 };
 
 const watchDev = () => {
-  gulp.watch(stylesAll, processStyles).on("change", browserSync.reload);
-  gulp.watch(htmlAll, processHTML).on("change", browserSync.reload);
-  gulp.watch(jsAll, processJS).on("change", browserSync.reload);
+  gulp.watch(styleFiles, processStyles).on("change", browserSync.reload);
+  gulp.watch(htmlFiles, processHTML).on("change", browserSync.reload);
+  gulp.watch(jsFiles, processJS).on("change", browserSync.reload);
+  gulp.watch(imageFiles, processImages).on("change", browserSync.reload);
 };
 
 browserSync.create();
@@ -73,7 +77,7 @@ browserSync.create();
 const initBrowserSync = () => {
   browserSync.init({
     server: {
-      baseDir: dist,
+      baseDir: distDir,
       serveStaticOptions: {
         extensions: ["html"],
       },
@@ -86,5 +90,5 @@ const initBrowserSync = () => {
 
 const jobs = [clean, processHTML, processStyles, processJS, processImages];
 
-module.exports.default = gulp.parallel(...jobs, initBrowserSync, watchDev);
-module.exports.build = gulp.series(...jobs);
+exports.default = gulp.parallel(...jobs, initBrowserSync, watchDev);
+exports.build = gulp.series(...jobs);
