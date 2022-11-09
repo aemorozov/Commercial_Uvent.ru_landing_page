e6cd6508f668cc935b7a9c7a7e9fd3fc046dddc1
aemorozov <mclenina19@gmail.com>
2022-11-09 16:48:08 +0400

    chore: add GULP

---
+++ gulpfile.js
@@ -0,0 +1,96 @@
+const gulp = require("gulp");
+const htmlMinify = require("gulp-htmlmin");
+const jsMinify = require("gulp-uglify");
+const imgagemin = require("gulp-imagemin");
+const { sync } = require("del");
+
+const autoprefixer = require("gulp-autoprefixer");
+const cssMinify = require("gulp-clean-css");
+const gcssmq = require("gulp-group-css-media-queries");
+const gulpSass = require("gulp-sass");
+const sass = require("sass");
+
+const browserSync = require("browser-sync");
+
+const distDir = "./dist/";
+const srcDir = "./src/";
+
+const htmlDir = "html/";
+const htmlFiles = `${srcDir}${htmlDir}*.html`;
+
+const jsDir = "js/";
+const jsFiles = `${srcDir}${jsDir}*.js`;
+
+const imgDir = "img/";
+const imgFiles = `${srcDir}${imgDir}**/*`;
+
+const stylesDir = "styles/";
+const styleFiles = `${srcDir}${stylesDir}*.+(css|scss)`;
+
+const processHTML = () => {
+  return gulp
+    .src(htmlFiles)
+    .pipe(htmlMinify({ collapseWhitespace: true, removeComments: true }))
+    .pipe(gulp.dest(distDir))
+    .pipe(browserSync.reload({ stream: true }));
+};
+
+const processJS = () => {
+  return gulp
+    .src(jsFiles)
+    .pipe(jsMinify())
+    .pipe(gulp.dest(`${distDir}${jsDir}`))
+    .pipe(browserSync.stream());
+};
+
+const processIMG = () => {
+  return gulp
+    .src(imgFiles)
+    .pipe(imgagemin())
+    .pipe(gulp.dest(`${distDir}${imgDir}`));
+};
+
+const gulpSassWorker = gulpSass(sass);
+
+const processStyle = () => {
+  return gulp
+    .src(styleFiles)
+    .pipe(gulpSassWorker().on("error", gulpSassWorker.logError))
+    .pipe(autoprefixer({ grid: true }))
+    .pipe(gcssmq())
+    .pipe(cssMinify())
+    .pipe(gulp.dest(`${distDir}${stylesDir}`))
+    .pipe(browserSync.stream());
+};
+
+const clean = async () => {
+  return sync(distDir, { force: true });
+};
+
+const watchDev = () => {
+  gulp.watch(styleFiles, processStyle).on("change", browserSync.reload);
+  gulp.watch(htmlFiles, processHTML).on("change", browserSync.reload);
+  gulp.watch(jsFiles, processJS).on("change", browserSync.reload);
+  gulp.watch(imgFiles, processIMG).on("change", browserSync.reload);
+};
+
+browserSync.create();
+
+const initBrowserSync = () => {
+  browserSync.init({
+    server: {
+      baseDir: distDir,
+      serveStaticOptions: {
+        extensions: ["html"],
+      },
+    },
+    port: 8080,
+    ui: { port: 8081 },
+    open: true,
+  });
+};
+
+const jobs = [clean, processHTML, processJS, processIMG, processStyle];
+
+exports.build = gulp.series(...jobs);
+exports.default = gulp.parallel(...jobs, initBrowserSync, watchDev);
