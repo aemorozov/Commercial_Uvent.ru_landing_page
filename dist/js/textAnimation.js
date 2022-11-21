8bc36395599d7223e510a1c83cf05de7ae92f6b4
aemorozov <mclenina19@gmail.com>
2022-11-21 18:33:28 +0400

    chore: fix textAnimation js

--- dist/js/textAnimation.js
+++ dist/js/textAnimation.js
@@ -1,15 +1,12 @@
 const h1_1 = document.querySelector(".h1_1"),
-  h1_2 = document.querySelector(".h1_2"),
-  h1_3 = document.querySelector(".h1_3"),
   h1_1Arr = h1_1.textContent.split("");
 let i = 0,
-  time = 90,
-  interval = 1.2;
+  time = 90;
 function firstLine() {
-  const e = setInterval(() => {
+  const t = setInterval(() => {
     void 0 !== h1_1Arr[i]
       ? ((h1_1.innerHTML += h1_1Arr[i]), i++)
-      : (clearInterval(e), (i = 0));
+      : (clearInterval(t), (i = 0));
   }, time);
 }
 firstLine(), (h1_1.innerHTML = "");
