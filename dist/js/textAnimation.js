7ef6582452f6de658a747b7746713f192ea5561d
aemorozov <mclenina19@gmail.com>
2022-11-21 18:36:02 +0400

    chore: set new time fore textAnimation.js

--- dist/js/textAnimation.js
+++ dist/js/textAnimation.js
@@ -1,7 +1,7 @@
 const h1_1 = document.querySelector(".h1_1"),
   h1_1Arr = h1_1.textContent.split("");
 let i = 0,
-  time = 90;
+  time = 70;
 function firstLine() {
   const t = setInterval(() => {
     void 0 !== h1_1Arr[i]
