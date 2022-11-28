14ab1c1421a8713845ecd067b2d30305399f64d6
aemorozov <mclenina19@gmail.com>
2022-11-28 17:28:56 +0400

    chore: wokr on animation text

--- src/js/textAnimation.js
+++ src/js/textAnimation.js
@@ -1,26 +1,58 @@
 const h1_1 = document.querySelector(".h1_1"); // забираем из HTML тег H1 с h1_1
+const h1_2 = document.querySelector(".h1_2"); // забираем из HTML тег H1 с h1_1
+const h1_3 = document.querySelector(".h1_3"); // забираем из HTML тег H1 с h1_1
 
 const h1_1Arr = h1_1.textContent.split(""); // превращаем вложенный текст в массив по символам
+const h1_2Arr = h1_2.textContent.split(""); // превращаем вложенный текст в массив по символам
+const h1_3Arr = h1_3.textContent.split(""); // превращаем вложенный текст в массив по символам
 
 let i = 0; // с 0-го значения начинается отсчёт массива для появления символов
-let time = 30; // задаём время между появлением символов в мс
+let time = 25; // задаём время между появлением символов в мс
+let interval = 2; // задаём интервал между строками
 
-firstLine(); // запускаем вторую строку через время, когда кончится первая
+let timeToSecondString = h1_1Arr.length * time * interval; // задаём время до появления второй строки используя длину первой строки
+let timeToThirdString = timeToSecondString + h1_2Arr.length * time * interval; // задаём время до появления второй строки используя длину первой строки
+
+firstLine(); // запускаем первую функцию (строку) сразу
+setTimeout(secondLine, timeToSecondString);
+setTimeout(thirdLine, timeToThirdString);
 
 h1_1.innerHTML = ""; // опустошаем вложенный текст в документе
+h1_2.innerHTML = ""; // опустошаем вложенный текст в документе
+h1_3.innerHTML = ""; // опустошаем вложенный текст в документе
 
 function firstLine() {
-  // создаём функцию для появления первой строки
   const intervalFirstLine = setInterval(() => {
-    // создаём переменную, в которую вкладываем функцию с интервалом
     if (h1_1Arr[i] !== undefined) {
-      // описываем условие, если в массиве под индексом i есть какие-то данные, то...
       h1_1.innerHTML += h1_1Arr[i]; // в тег H1 вкладываем символ из массива, сохраняя ранее вложенные данные
       i++; // увеличиваем номер индекса для следующей проверки
     } else {
-      // а если значение под индексом не определено, то...
       clearInterval(intervalFirstLine); // останавливаем выполнение функции
       i = 0; // присваиваем i снова 0
     }
   }, time); // задаем время для каждой итерации
+}
+
+function secondLine() {
+  let intervalSecondLine = setInterval(() => {
+    if (h1_2Arr[i] !== undefined) {
+      h1_2.innerHTML += h1_2Arr[i];
+      i++;
+    } else {
+      clearInterval(intervalSecondLine);
+      i = 0;
+    }
+  }, time);
+}
+
+function thirdLine() {
+  let intervalThirdLine = setInterval(() => {
+    if (h1_3Arr[i] !== undefined) {
+      h1_3.innerHTML += h1_3Arr[i];
+      i++;
+    } else {
+      clearInterval(intervalThirdLine);
+      i = 0;
+    }
+  }, time);
 }
