0e0f3a2faca93d0ec2708c712961f4052cae6120
aemorozov <mclenina19@gmail.com>
2022-12-07 15:24:17 +0400

    chore: clean index.html

--- src/js/textAnimationWithOpacity.js
+++ src/js/textAnimationWithOpacity.js
@@ -17,9 +17,12 @@ function textAnimationWithOpacity() {
 
   function addSpansWithOpacity0() {
     for (let rowNumber = 0; rowNumber < h1Array.length; rowNumber++) {
-      let textContent = h1Array[rowNumber].textContent;
-      let textContentAfterSplit = textContent.split("");
-      let textContentLength = textContent.length;
+      const textContent = h1Array[rowNumber].textContent;
+      if (!textContent) {
+        return
+      }
+      const textContentAfterSplit = textContent.split("");
+      const textContentLength = textContent.length;
       h1Array[rowNumber].innerHTML = "";
 
       for (let i = 0; i < textContentLength; i++) {
@@ -34,7 +37,7 @@ function textAnimationWithOpacity() {
 
   function addOpacity1() {
     const forCleanInterval = setInterval(() => {
-      let symbol = document.querySelector(`#${prefixForID}${symbolID}`);
+      const symbol = document.querySelector(`#${prefixForID}${symbolID}`);
       if (symbol) {
         symbol.classList.add("opacity-1-styling");
         symbolID++;
---
+++ src/ts/textAnimationWithOpacity.ts
@@ -0,0 +1,44 @@
+textAnimationWithOpacityTS();
+
+function textAnimationWithOpacityTS(): void {
+  type OnlyPrefixSymbol = "symbol-"
+  type Only100 = 100
+  
+  const h1Array: NodeListOf<Element> = document.querySelectorAll(".tagline-h1");
+  const prefixForID: OnlyPrefixSymbol = "symbol-";
+  const speed: Only100 = 100;
+  let symbolID: number = 0;
+
+  addSpansWithOpacity0();
+  addOpacity1();
+
+  function addSpansWithOpacity0(): void {
+    for (let rowNumber = 0; rowNumber < h1Array.length; rowNumber++) {
+      const textContent: string | null = h1Array[rowNumber].textContent;
+      if (!textContent) {
+          return
+      }
+      const textContentAfterSplit: Array<String> = textContent.split("");
+      const textContentLength: number = textContent.length;
+      h1Array[rowNumber].innerHTML = "";
+
+      for (let i = 0; i < textContentLength; i++) {
+        h1Array[
+          rowNumber
+        ].innerHTML += `<span class="opacity-0-styling" id="${prefixForID}${symbolID}">${textContentAfterSplit[i]}</span>`;
+        symbolID++;
+      }
+    }
+    symbolID = 0;
+  }
+
+  function addOpacity1(): void {
+    const forCleanInterval: NodeJS.Timer = setInterval((): void => {
+      let symbol: Element | null = document.querySelector(`#${prefixForID}${symbolID}`);
+      if (symbol) {
+        symbol.classList.add("opacity-1-styling");
+        symbolID++;
+      } else clearInterval(forCleanInterval);
+    }, speed);
+  }
+}
