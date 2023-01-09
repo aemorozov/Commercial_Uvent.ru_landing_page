496573114a762a1f5f98c17cb05c64af4359760b
aemorozov <mclenina19@gmail.com>
2023-01-09 17:37:38 +0400

    chore: add new shift for slider

--- src/ts/sliderButtons.ts
+++ src/ts/sliderButtons.ts
@@ -2,7 +2,7 @@ const forSlider: HTMLElement | null = document.querySelector('.for-slider')
 const fieldServices: HTMLElement | null = document.querySelector('.uslugi-for-circles')
 const buttonLeft: HTMLElement | null = document.querySelector('.arrow-left')
 const buttonRight: HTMLElement | null = document.querySelector('.arrow-right')
-const shift = 250
+const shift = 500
 let scrollLeftVar = forSlider?.scrollLeft
 
 forSlider?.addEventListener('scroll', scrollingX)
