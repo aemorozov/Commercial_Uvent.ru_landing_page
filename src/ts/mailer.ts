bd48e568623f51769127b4b5b8038c9c6c8f5858
aemorozov <mclenina19@gmail.com>
2023-01-31 19:50:37 +0400

    chore: add form animation

--- src/ts/mailer.ts
+++ src/ts/mailer.ts
@@ -1,7 +1,13 @@
 const emailjs = require('@emailjs/browser');
 const btn = document.querySelector<HTMLInputElement>(".button");
+const contactBlock: HTMLElement | null = document.querySelector('.feedback-flex')
+const checkCircle: HTMLElement | null = document.querySelector('.check-circle')
+const groupCircle: HTMLElement | null = document.querySelector('.group-circle')
+const thanks: HTMLElement | null = document.querySelector('.thanks')
+const thanksText: HTMLElement | null = document.querySelector('.thanks-text')
+const thanksTextBold: HTMLElement | null = document.querySelector('.thanks-text-bold')
 
-emailjs.init("D32-TRfUefWjSF0BO");
+emailjs.init("0KeYNjBhOnI3x-_ra");
 
 document.getElementById('form')?.addEventListener('submit', function(event) {
    event.preventDefault();
@@ -10,15 +16,29 @@ document.getElementById('form')?.addEventListener('submit', function(event) {
 
    btn.value = 'Отправляется...';
 
-   const serviceID = 'default_service';
-   const templateID = 'template_j2n3uuh';
+   const serviceID = 'service_zld1lus';
+   const templateID = 'template_k5xltof';
 
    emailjs.sendForm(serviceID, templateID, this)
     .then(() => {
-      btn.value = 'Отправить';
-      alert('Sent!');
+      contactBlock?.classList.add('animation-go-up')
+      setTimeout(() => {
+        contactBlock?.classList.add('display-none')
+        checkCircle?.classList.remove('display-none')
+        groupCircle?.classList.remove('display-none')
+        thanks?.classList.remove('display-none')
+        thanksText?.classList.remove('display-none')
+        thanksTextBold?.classList.remove('display-none')
+      }, 300)
+      setTimeout(() => {
+        checkCircle?.classList.add('thanks-go-up')
+        groupCircle?.classList.add('thanks-go-up')
+        thanks?.classList.add('thanks-go-up')
+        thanksText?.classList.add('thanks-go-up')
+        thanksTextBold?.classList.add('thanks-go-up')
+      }, 400)
     }, (err: Error) => {
       btn.value = 'Отправить';
-      alert(JSON.stringify(err));
+      alert('Произошла какая-то ошибка! Пожалуйста, напишите нам на почту uvent2020@mail.ru или позвоните +7 916 115 15 30' + err);
     });
 });
